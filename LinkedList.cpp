d0d32fe21ad043ef87ab500057ab8957dd8704ab
Akash sahu <asahu7744@gmail.com>
2021-01-13 17:11:09 +0430

    LL

---
+++ LinkedList.cpp
@@ -0,0 +1,62 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+void Display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 2);
+    insertAtHead(head, 4);
+    insertAtHead(head, 6);
+    insertAtTail(head, 7);
+    insertAtTail(head, 8);
+    Display(head);
+    return 0;
+}
