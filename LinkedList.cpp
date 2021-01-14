8f3049c71fb9a5feee4a34ef48f58701ba8fb340
Akash sahu <asahu7744@gmail.com>
2021-01-14 12:46:51 +0430

    link list complete

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -49,6 +49,153 @@ void Display(node *head)
     }
     cout << "NULL" << endl;
 }
+
+bool search(node *head, int key)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+void deleteAtHead(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+    delete todelete;
+}
+
+void deletion(node *&head, int val)
+{
+    node *temp = head;
+    if (temp->data == val)
+    {
+        deleteAtHead(head);
+        return;
+    }
+
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
+}
+
+node *Reverse(node *head)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+
+    while (currptr != NULL)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+    return prevptr;
+}
+
+node *reverseRecursive(node *head)
+{
+
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+
+    node *newhead = reverseRecursive(head->next);
+    head->next->next = head;
+    head->next = NULL;
+
+    return newhead;
+}
+
+node *Reversek(node *head, int k)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+    int count = 0;
+    while (currptr != NULL && count < k)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
+    }
+    if (nextptr != NULL)
+    {
+        head->next = Reversek(nextptr, k);
+    }
+
+    return prevptr;
+}
+
+void makeCycle(node *&head, int pos)
+{
+    node *temp = head;
+    node *startnode;
+    int count = 1;
+    while (temp->next != NULL)
+    {
+        if (count == pos)
+        {
+            startnode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = startnode;
+}
+int detectCycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+    while (fast->next != NULL && fast != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void removeCycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+
+    do
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    } while (slow != fast);
+
+    fast = head;
+    while (fast->next != slow->next)
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    slow->next = NULL;
+}
+
 int main()
 {
     node *head = NULL;
@@ -57,6 +204,10 @@ int main()
     insertAtHead(head, 6);
     insertAtTail(head, 7);
     insertAtTail(head, 8);
+    makeCycle(head, 4);
+    cout << detectCycle(head) << endl;
+    removeCycle(head);
+    cout << detectCycle(head) << endl;
     Display(head);
     return 0;
 }
